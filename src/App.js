d9b1ee98f6e5466ac2f97f7d0acbb23cc0dc1969
drebyrd715 <drebyrd715@gmail.com>
2023-11-30 09:42:30 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Movietitles from './components/Movietitles';
 import Moretitles from './components/Moretitles';
 import Somemoretitle from './components/Somemoretitle';
 import Lastimage from './components/Lastimage';
-
+import Header from './components/Header';
 function App() {
   return (
     <>
+    <Header/>
     <Seeamovie/>
     <Movietitles/>
     <Moretitles/>
